283c696132d816bb74d6d68bbd8b139b23d8464c
Alen Pelin <alen.pelin@gmail.com>
2024-07-30 14:35:46 +1000

    fix: full cycle

--- update-env.js
+++ update-env.js
@@ -8,6 +8,8 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const res = {...process.env};
+
 const env = (name) => new Promise((resolve) => {
     if (process.env.UNIFORM_API_KEY && name === 'UNIFORM_API_KEY') {
         resolve();
@@ -15,18 +17,19 @@ const env = (name) => new Promise((resolve) => {
     }
 
     rl.question(`Please specify ${name}: `, (answer) => {
-        process.env[name] = answer;
+        res[name] = answer;
         resolve(answer);
-        rl.close();
     });
 });
 
-Promise.all([env('UNIFORM_API_KEY'), env('UNIFORM_PROJECT_ID')]).then(() =>
-{
-    const text = `UNIFORM_CLI_BASE_URL=${process.env.UNIFORM_CLI_BASE_URL ?? 'https://uniform.app'}
-UNIFORM_API_KEY=${process.env.UNIFORM_API_KEY}
-
-UNIFORM_PROJECT_ID=${process.env.UNIFORM_PROJECT_ID}
+env('UNIFORM_API_KEY').then(x => {
+    env('UNIFORM_PROJECT_ID').then(() => {
+        const text = `# GENERATED
+UNIFORM_CLI_BASE_URL=${res.UNIFORM_CLI_BASE_URL ?? 'https://uniform.app'}
+UNIFORM_API_KEY=${res.UNIFORM_API_KEY}
+UNIFORM_PROJECT_ID=${res.UNIFORM_PROJECT_ID}
 `;
-    writeFileSync('./.env', text);
-})
+        writeFileSync('./.env', text);
+        rl.close();
+    });
+});
