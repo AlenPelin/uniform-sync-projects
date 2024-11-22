c67d691dc2901ffe32583f3bc472e627ddfac50f
Alen Pelin <alen.pelin@gmail.com>
2024-11-22 22:33:05 +1100

    chore: assets disabled by default

--- uniform.config.ts
+++ uniform.config.ts
@@ -33,10 +33,8 @@ const config: CLIConfiguration = {
       projectMapDefinition: { },
       projectMapNode:       { },
 
-      // assets (may take a lot of time, consider disabling)
-      asset: {},
-
       /* DISABLED BY DEFAULT !!! 
+      // assets (may take a lot of time, consider disabling)
       asset:                { },
       */
       
