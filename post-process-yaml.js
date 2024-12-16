fc88a601ba806ca05f9acffe47ab49b59d3101a0
Alen Pelin <alen.pelin@gmail.com>
2024-12-16 17:32:11 +1100

    fix: EMFILE: too many open files

--- post-process-yaml.js
+++ post-process-yaml.js
@@ -111,11 +111,12 @@ function getYamlContent(data, filePath) {
 
 // Function to remove properties and sort keys
 function removePropertiesAndSort(filePath) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading file ${filePath}:`, err);
-            return;
-        }
+    // fs.readFile(filePath, 'utf8', (err, data) => {
+    //     if (err) {
+    //         console.error(`Error reading file ${filePath}:`, err);
+    //         return;
+    //     }
+    let data = fs.readFileSync(filePath, { encoding: 'utf8' });
 
         console.log('Post-processing file ' + filePath);
         const crLf = true;
@@ -178,28 +179,29 @@ function removePropertiesAndSort(filePath) {
         } catch (e) {
             console.error(`Failed to prost-process YAML file ${filePath}:`, e);
         }
-    });
+    // });
 }
 
 // Function to traverse directories and find .yaml files
-function traverseDirectory(dir) {
-    fs.readdir(dir, { withFileTypes: true }, (err, files) => {
-        if (err) {
-            console.error(`Error reading directory ${dir}:`, err);
-            return;
-        }
-
+function traverseDirectory(dir, notFirstLevelDir) {
+    // fs.readdir(dir, { withFileTypes: true }, (err, files) => {
+    //     if (err) {
+    //         console.error(`Error reading directory ${dir}:`, err);
+    //         return;
+    //     }
+    const files = fs.readdirSync(dir, { withFileTypes: true })
         files.forEach(file => {
             const filePath = path.join(dir, file.name);
             if (file.isDirectory()) {
                 // If it's a directory, recurse into it
-                traverseDirectory(filePath);
-            } else if (file.isFile() && path.extname(file.name) === '.yaml') {
+                traverseDirectory(filePath, true);
+            } else if (notFirstLevelDir && file.isFile() && path.extname(file.name) === '.yaml') {
                 // If it's a .yaml file, process it
                 removePropertiesAndSort(filePath);
             }
         });
     });
+    //});
 }
 
 // Start traversing from the root directory
